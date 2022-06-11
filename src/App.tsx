1ddfaf1b18be58242bd06a4f32655f883ad0edab
thayron Matos <thayron.f3@gmail.com>
2022-06-11 16:34:34 -0300

    Concluded page initiral. (No task informed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
 import "./global.css";
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
 import { Input } from "./components/Input";
+import { useState } from "react";
+import ClipboardText from "./assets/Clipboard.svg";
 
 function App() {
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [tasksCompleted, setTasksCompleted] = useState<number>(0);
+
+  function handleChangeInput(handleChangeInput: any) {
+    const newTask = {
+      id: Math.random() * (50 - 1 + 1),
+      task: handleChangeInput,
+      isConcluded: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    onCompleteTask();
+  }
+
+  function onCompleteTask() {
+    setTasksCompleted(0);
+  }
+
   return (
     <div className="App">
       <Header />
-      <Input />
+      <Input handleChangeInput={handleChangeInput} />
+      <div className={styles.container}>
+        <div className={styles.historyTasks}>
+          <div className={styles.createdTasks}>
+            <p> Tarefas criadas</p>
+            <p className={styles.resultStatusTasks}>{tasks.length}</p>
+          </div>
+
+          <div className={styles.concludedTasks}>
+            <p> Concluídas</p>
+            <p className={styles.resultStatusTasks}>{tasksCompleted}</p>
+          </div>
+        </div>
+
+        <span className={styles.line}></span>
+
+        {tasks.length === 0 ? (
+          <div className={styles.containerTask}>
+            <div className={styles.notAlreadyExistsTasks}>
+              <img src={ClipboardText} alt="Clipboard" />
+              <p>Você ainda não tem tarefas cadastradas</p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </div>
+        ) : (
+          <div>asdfasdf</div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,33 @@
 import { PlusCircle } from "phosphor-react";
-import styles from './Input.module.css';
+import { ChangeEvent, useState } from "react";
+import styles from "./Input.module.css";
 
 interface IInputrops {
-    placeholder?: string;
+  placeholder?: string;
+  handleChangeInput: (valor:any) => void;
 }
 
 const defaultValueLabel = "Adicione uma nova tarefa";
 
-export function Input({ placeholder = defaultValueLabel }: IInputrops) {
+export function Input({ placeholder = defaultValueLabel, handleChangeInput }: IInputrops) {
+  const [valueInput, setValueInput] = useState('');
+
+  function handleChangeInputFunction(event: ChangeEvent<HTMLInputElement>) {
+    setValueInput(event.target.value);
+  }
+
+  function onSubmit() {
+    handleChangeInput(valueInput);
+  }
+
   return (
-    <div className={styles.container} >
-      <input type="text" placeholder={placeholder} />
-      <button>Criar <PlusCircle size={22}/></button>
-    </div>
+    <>
+      <div className={styles.container}>
+        <input type="text" placeholder={placeholder} onChange={handleChangeInputFunction} />
+        <button onClick={onSubmit}>
+          Criar <PlusCircle size={22} />
+        </button>
+      </div>
+    </>
   );
 }
